3d5650fadb8148089a328ba8c82bd731a8ebde6a
Omar Attaalla <oatta001@ucr.edu>
2022-02-10 11:57:17 -0800

    Add placeAt : Square attribute and array of Square objects to ChessPiece abstract class

--- header/ChessPiece.h
+++ header/ChessPiece.h
@@ -1,9 +1,13 @@
 #ifndef CHESSPIECE_H
 #define CHESSPIECE_H
 
+#include "Square.h"
+
 class ChessPiece {
     private:
         bool color;
+        Square moveableSquares[64]; //64 since the maximum possible squares are 64. However this could be less
+        Square placeAt; //The square where the piece is located
     public:
         virtual void Moves() = 0;
         virtual void pieceDeath() = 0;

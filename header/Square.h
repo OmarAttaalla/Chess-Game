70ee50d9f693a8c0ce3836212201bb77c585dd3b
Omar Attaalla <oatta001@ucr.edu>
2022-02-10 11:46:27 -0800

    Add Square.h with Square class decleration

---
+++ header/Square.h
@@ -0,0 +1,14 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+
+class Square {
+    private:
+        int row;
+        int column;
+    public:
+        int get_row();
+        int get_column();
+};
+
+#endif

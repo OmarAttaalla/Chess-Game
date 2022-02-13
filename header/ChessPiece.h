2855d78b397466df58fb239dba01f275663ce4cc
Omar Attaalla <oatta001@ucr.edu>
2022-02-12 21:47:31 -0800

    Change moveableSquares to vector implementation and pass in Pieces vector to scan function

--- header/ChessPiece.h
+++ header/ChessPiece.h
@@ -2,15 +2,16 @@
 #define CHESSPIECE_H
 
 #include "Square.h"
+#include <vector>
 
 class ChessPiece {
     private:
         bool color;
-        Square moveableSquares[64]; //64 since the maximum possible squares are 64. However this could be less
+        std::vector<Square> moveableSquares; //vector of Square moveable locations
         Square placeAt; //The square where the piece is located
     public:
         virtual void Moves() = 0;
-        virtual void scan() = 0;
+        virtual void scan(std::vector<ChessPiece*> &Pieces) = 0;
         virtual void pieceDeath() = 0;
 };
 

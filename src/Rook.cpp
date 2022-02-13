bff1bed29c9c6da14a16dd5186ce60f09f0cd4b3
Omar Attaalla <oatta001@ucr.edu>
2022-02-13 11:02:03 -0800

    Add Rook scan() function implementation

--- src/Rook.cpp
+++ src/Rook.cpp
@@ -28,3 +28,21 @@ void Rook::straightMove(bool d, int i, int pieceLocations[8][8]) {
         this->add_move(newMove);
     }
 }
+
+void Rook::scan(std::vector<ChessPiece*> &Pieces) {
+    
+    int pieceLocations[8][8] = {0}; //Initialize 2d array of Chess Board to 0 (no piece at that location)
+
+    for (int i =0; i < Pieces.size(); ++i) { // traverse over Pieces vector and assign the Pieces to their pieceLocation
+        if (Pieces.at(i)->get_color()) {
+            pieceLocations[Pieces.at(i)->get_placeAt().get_column()][Pieces.at(i)->get_placeAt().get_row()] = 1; //assign 1 to locations that contain a white piece
+        } else {
+            pieceLocations[Pieces.at(i)->get_placeAt().get_column()][Pieces.at(i)->get_placeAt().get_row()] = 2; //assign 2 to locations that contain a black piece
+        }
+    }
+
+    straightMove(true, 1, pieceLocations); // add all moves available to the right of the rook
+    straightMove(false, 1, pieceLocations); // add all moves available above the rook
+    straightMove(true, -1, pieceLocations); // add all moves available to the left of the rook
+    straightMove(false, -1, pieceLocations); // add all moves available under the rook
+}

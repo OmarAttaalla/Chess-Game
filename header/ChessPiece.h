5d01c76f7ad2125bffd35abfc293aa599ac8a8ac
Omar Attaalla <oatta001@ucr.edu>
2022-02-19 11:56:50 -0800

    Add setters for color and placeAt

--- header/ChessPiece.h
+++ header/ChessPiece.h
@@ -14,7 +14,9 @@ class ChessPiece {
         virtual void scan(std::vector<ChessPiece*> &Pieces) = 0; //function that scans available moves for the chess piece
         virtual void pieceDeath() = 0;
         bool get_color(){ return color;}
+        void set_color(bool newColor){ color = newColor;} //Set the color of the piece
         Square get_placeAt(){ return placeAt;}
+        void set_placeAt(Square newPlaceAt) {placeAt = newPlaceAt;} //Set the square where the piece is located
         void add_move(Square newMove){ moveableSquares.push_back(newMove);} //adds move to moveableSquares vector
 };
 

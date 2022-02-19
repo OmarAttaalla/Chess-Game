14f36f7e5e3f27fe7bdfec1b7a5e2f48baac480e
Omar Attaalla <oatta001@ucr.edu>
2022-02-19 12:03:03 -0800

    Add MoveAlg object that points to the movement algorithm object

--- header/Rook.h
+++ header/Rook.h
@@ -6,11 +6,12 @@
 
 class Rook : public ChessPiece {
     private:
-        
+        MoveAlg* straightMove; //pointer to object that represents the movement algorithm needed for Rook (StraightMove alg)
     public:
         Rook(bool newColor, Square newPlaceAt) {
             set_color(newColor);
             set_placeAt(newPlaceAt);
+            straightMove = new StraightMove;
         }
         virtual void Moves();
         virtual void scan(std::vector<ChessPiece*> &Pieces);

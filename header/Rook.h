c92cbb6dac8086284b725dcc27d51114a27e92ae
Omar Attaalla <oatta001@ucr.edu>
2022-02-19 11:58:39 -0800

    Add Rook constructor

--- header/Rook.h
+++ header/Rook.h
@@ -2,16 +2,19 @@
 #define ROOK_H
 
 #include "ChessPiece.h"
+#include "StraightMove.h"
 
 class Rook : public ChessPiece {
     private:
         
     public:
+        Rook(bool newColor, Square newPlaceAt) {
+            set_color(newColor);
+            set_placeAt(newPlaceAt);
+        }
         virtual void Moves();
         virtual void scan(std::vector<ChessPiece*> &Pieces);
-        virtual void pieceDeath();
-        //Helper function that increments up,down,left,right until a ChessPiece is found or it reaches a bound
-        void straightMove(bool d, int i, int pieceLocations[8][8]); //Passes in column/row axis, the move increment (1 or -1), and 2d array of the locations of all ChessPieces
+        virtual void pieceDeath(); 
 };
 
 

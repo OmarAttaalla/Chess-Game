007a99008b15ae5e6d92794404722f1200acddc4
Omar Attaalla <oatta001@ucr.edu>
2022-02-26 13:19:37 -0800

    Update Chess Piece

--- header/ChessPiece.h
+++ header/ChessPiece.h
@@ -2,22 +2,25 @@
 #define CHESSPIECE_H
 
 #include "Square.h"
+#include "ChessBoard.h"
 #include <vector>
+#include <iostream>
 
 class ChessPiece {
     private:
         bool color;
-        std::vector<Square> moveableSquares; //vector of Square moveable locations
-        Square placeAt; //The square where the piece is located
+        std::vector<Square*> moveableSquares; //vector of Square moveable locations
+        Square* placeAt; //The square where the piece is located
     public:
         virtual void Moves() = 0;
-        virtual void scan(std::vector<ChessPiece*> &Pieces) = 0; //function that scans available moves for the chess piece
+        virtual void scan(ChessBoard &Board) = 0; //function that scans available moves for the chess piece
         virtual void pieceDeath() = 0;
         bool get_color(){ return color;}
         void set_color(bool newColor){ color = newColor;} //Set the color of the piece
-        Square get_placeAt(){ return placeAt;}
-        void set_placeAt(Square newPlaceAt) {placeAt = newPlaceAt;} //Set the square where the piece is located
-        void add_move(Square newMove){ moveableSquares.push_back(newMove);} //adds move to moveableSquares vector
+        Square* get_placeAt(){ return placeAt;}
+        void set_placeAt(Square* newPlaceAt) {placeAt = newPlaceAt;} //Set the square where the piece is located
+        void add_move(Square* newMove){ moveableSquares.push_back(newMove);} //adds move to moveableSquares vector
+        std::vector<Square*> get_moveableSquares() {return moveableSquares;}
 };
 
 #endif

c4eb8a6589a1176367238ca263782272802dd9da
ELW123 <ethanwan5@gmail.com>
2022-03-01 20:06:18 -0800

    Revert "Oatta001/rook movement"

--- src/Rook.cpp
+++
@@ -1,9 +0,0 @@
-#include "../header/Rook.h"
-
-void Rook::scan(ChessBoard &Board) {
-    
-    //Add all Straight Path Moves
-    for (int i =0; i < 4; ++i) {
-        straightScan->Scan(this, i, Board); // add all straight moves
-    }
-}

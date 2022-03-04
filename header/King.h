9c371420d6c5a796e81f99eeb4bbded4f48f0b2e
Omar Attaalla <oatta001@ucr.edu>
2022-03-03 20:13:06 -0800

    Add King.h

---
+++ header/King.h
@@ -0,0 +1,22 @@
+#ifndef KING_H
+#define KING_H
+
+#include "ChessPiece.h"
+#include "KingScan.h"
+
+class King : public ChessPiece {
+    private:
+        ScanAlg* kingScan;
+    public:
+        King(bool newColor, Square* newPlaceAt) {
+            set_color(newColor);
+            set_placeAt(newPlaceAt);
+            kingScan = new KingScan;
+        }
+        virtual void moves(Square* sq) {}
+        virtual void scan(ChessBoard &Board);
+        virtual void pieceDeath() {}
+};
+
+
+#endif

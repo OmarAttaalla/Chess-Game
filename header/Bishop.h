029be19f393208e9ba1293c16157a5492ebb6892
Omar Attaalla <oatta001@ucr.edu>
2022-03-09 15:59:08 -0800

    Add Bishop destructor

--- header/Bishop.h
+++ header/Bishop.h
@@ -14,6 +14,7 @@ class Bishop : public ChessPiece {
 
     public:
         Bishop(bool newColor, Square* newPlaceAt);
+        ~Bishop();
         virtual void scan(ChessBoard &Board);
         virtual void pieceDeath() {}
 };

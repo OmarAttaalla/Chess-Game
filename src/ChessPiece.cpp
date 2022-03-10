ef83c32737445dd61723bc77ffa4ec63bec717ec
Omar Attaalla <oatta001@ucr.edu>
2022-03-09 17:34:45 -0800

    Set old location to nullptr

--- src/ChessPiece.cpp
+++ src/ChessPiece.cpp
@@ -6,8 +6,10 @@ void ChessPiece::moves(Square* sq, ChessBoard &Board) {
             if (sq->getPiece() != nullptr) {
                 Board.removePiece(sq->getPiece());
             }
+            placeAt->setPiece(nullptr); //set the current location to blank
             moveableSquares.at(i)->setPiece(this);
             placeAt = moveableSquares.at(i);
+
             clear_moveableSquares();
             return;
         }
